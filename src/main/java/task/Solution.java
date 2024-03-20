11b11d7b618ec970c3d841482a447e91972553d7
IgorZaharov1987 <igorzaharov1987@gmail.com>
2024-03-20 21:53:02 +0200

    add fixes

--- src/main/java/task/Solution.java
+++ src/main/java/task/Solution.java
@@ -28,7 +28,7 @@ public class Solution {
         }
 
         if (duplicates.isEmpty()) {
-            throw new IllegalArgumentException("No repeated words");
+            throw new RuntimeException("No repeated words");
         }
 
         for (String word : duplicates) {
@@ -37,7 +37,9 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        String text = "The enormous room on the ground floor faced towards the north. Cold for all the summer beyond the panes, for all the tropical heat of the room itself, a harsh thin light glared through the windows, hungrily seeking some draped lay figure, some pallid shape of academic gooseflesh, but finding only the glass and nickel and bleakly shining porcelain of a laboratory. Wintriness responded to wintriness. The overalls of the workers were white, their hands gloved with a pale corpse-colored rubber. The light was frozen, dead, a ghost. Only from the yellow barrels of the microscopes did it borrow a certain rich and living substance, lying along the polished tubes like butter, streak after luscious streak in long recession down the worktables.";
-        Solution.findDuplicates(text);
+        String initialText = "The enormous room on the ground floor faced towards the north. Cold for all the summer beyond the panes, for all the tropical heat of the room itself, a harsh thin light glared through the windows, hungrily seeking some draped lay figure, some pallid shape of academic gooseflesh, but finding only the glass and nickel and bleakly shining porcelain of a laboratory. Wintriness responded to wintriness. The overalls of the workers were white, their hands gloved with a pale corpse-colored rubber. The light was frozen, dead, a ghost. Only from the yellow barrels of the microscopes did it borrow a certain rich and living substance, lying along the polished tubes like butter, streak after luscious streak in long recession down the worktables.";
+        String removeFirstSentence = initialText.substring(initialText.indexOf(".") + 1).trim();
+
+        Solution.findDuplicates(removeFirstSentence);
     }
 }
